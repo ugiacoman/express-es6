6b78898673bfca8d2f1c08630840303dc382f4d0
Ulises Giacoman <ulises.giacoman@gmail.com>
2017-04-22 12:36:51 -0400

    update doc

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Routes
 // app.use('/', routes)
 
-// Catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found')
   err.status = 404
